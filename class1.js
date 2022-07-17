419c200ec66a35d2707d4e60d1526ccfb57900a4
programmerArif <arifulislamwasim2017@gmail.com>
2022-07-17 11:25:36 +0600

    class 1 done

--- class1.js
+++ class1.js
@@ -1,3 +1,22 @@
-const myNumber = -5;
-const output = Math.abs(myNumber);
-console.log(output)
+// const number1 = -5;
+// const output1 = Math.abs(number1);
+// console.log(output1)
+
+// const number2 = 2.1521;
+// const output2 = Math.ceil(number2);
+// console.log(output2);
+
+// const number3 = 2.1521;
+// const output3 = Math.floor(number3);
+// console.log(output3);
+
+
+// const number4 = Math.random() * 50;
+// const output4 = Math.round(number4);
+// console.log(output4);
+
+for (var i = 1; i <= 5; i++) {
+    const number5 = Math.random() * 10;
+    const output5 = Math.round(number5);
+    console.log(output5);
+}
